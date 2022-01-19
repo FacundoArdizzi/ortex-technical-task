c049c961696ea3d398db96f59fc783ed4d05a2d1
FacundoArdizzi <ardizzifacundo@gmail.com>
2022-01-19 01:35:44 -0300

    code cleaning

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import styles from './Footer.module.css'
 
 const Footer = () => (
-  <h1 className='footer'>
+  <h1 className={styles.footer}>
     <a target='_blank' href='https://www.linkedin.com/in/facundo-ardizzi/' rel="noreferrer" >By Facundo Ardizzi</a>
   </h1>
 )
