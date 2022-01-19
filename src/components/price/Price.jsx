ca2c6f4e9323619c9a709ea8e5b800ef48cc2abd
FacundoArdizzi <ardizzifacundo@gmail.com>
2022-01-19 02:40:53 -0300

    loader

--- src/components/price/Price.jsx
+++ src/components/price/Price.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import styles from './Price.module.css'
+import Loader from '../loader/Loader'
 
-const Price = ({price, time}) => (
-  <div className={styles.price} >
-    <h3>EUR/USD</h3>
-    <h2>{price}</h2>
-    <h3>{time}</h3>
-  </div>
-)
+const Price = ({price, time}) => {
+
+  return ( 
+    <>
+    {price ? (
+      <div className={styles.price}>
+        <h3>EUR/USD</h3>
+        <h2>{price}</h2>
+        <h3>{time}</h3>
+      </div>
+    ) : <div className={styles.ld}><Loader /></div>}
+    </>
+  )
+}
+
 
 export default Price
